ce5cbea8d3ca1775d6ba7a0ab5090f5456513f8f
gayoung lee <tjgo95@naver.com>
2018-01-15 15:01:30 +0900

    api flask server

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
-
+#-*- coding: utf-8 -*-
 from flask_restful import Resource,Api
 from flask import Flask, redirect, url_for, request
-import nltk
-#from flask.ext.jsonpify import jsonify
-from success import success
-  
-@app.route('/nltk', methods = ['POST', 'GET'])
-def nltk_text():
-     #   def get(self):
-         if request.method == 'POST':
-                 user = request.form['tag']
-                 print("post methods")
-                 return redirect(url_for('success', text = user))
-              #  return {'hello':'world'}
-         else:
-                 user = request.args.get('tag')
-                 print("get methods")
-                 return redirect(url_for('success', text = user))
-               # return {'hello':'world'}
-#api.add_resource(success,'/success/<text>')
-#api.add_resource(nltk_text,'/nltk')
+import konlp.asp.asp as A
+import json
+from flask_jsonpify import jsonify
+import konlp.tokenizer.wordcount as wc
 
-if __name__ == '__main__':
-        app.run(host = '0.0.0.0', debug = True)
+app = Flask(__name__)
+
+@app.route('/success/autospacing/<text>')
+def success1(text):
+       q = A.KltAsp()
+       q.dic_init('/usr/local/asp_dic')
+       print("------------------" + text.encode('utf8'))
+       str_ = q.asp(text.encode("utf8"))
+     #  return(str_)
+       it= json.dumps(str_, ensure_ascii=False, encoding ='utf-8') 
+       return it
+
+@app.route('/success/wordcounting/<text>')
+def success2(text):
+       q1 = wc.WordCount()
+       str_1 = q1.wordcount(text.encode('utf8'))
+      # return "%s" %str_1
+       it1 = json.dumps(str_1, ensure_ascii=False, encoding='utf-8')
+       return it1
+
+@app.route('/nltk/autospacing',methods = ['POST','GET'])
+def get_text1():
+       if request.method=='POST':
+             user = request.form['space']
+             return redirect(url_for('success1', text = user))
+       else :
+             user = request.args.get('space')
+             return redirect(url_for('success1',text= user))
+@app.route('/nltk/wordcounting', methods = ['POST'])
+def get_text2():
+       if request.method == 'POST':
+             user = request.form['count']
+             return redirect(url_for('success2' , text = user))
+if __name__ == '__main__' :
+       app.run(host='0.0.0.0', debug=True)
